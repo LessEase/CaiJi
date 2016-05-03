af22900edf25c806f7afc273f2cf0210f555c7c6
LessEase <chshaoyi7@gmail.com>
2016-05-03 22:56:25 +0800

    user feature

---
+++ code/user_feature/csy_count_user_feature.py
@@ -0,0 +1,23 @@
+#/usr/bin/python
+#_*_coding:utf-8_*_
+
+import cPickle
+
+class UserFeature(object):
+	
+	def __init__(self):
+		self.specific_location_feature = dict()
+		self.specific_merchant_feature = dict()
+		self.total_bought = 0
+	
+
+if __name__== '__main__':
+	
+	file = "" 
+
+	with open(file, 'r') as fin:
+		for line in fin:
+
+
+	
+
