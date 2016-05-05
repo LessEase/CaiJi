e119e1560c52ed56f04ec0c07d9915f3e6824bbd
root <root@sjs_86_171.sogou-in.domain>
2016-05-05 22:00:43 +0800

    repair

--- code/conduct_data/run_gen_train_data.sh
+++ code/conduct_data/run_gen_train_data.sh
@@ -1,8 +1,8 @@
 function Gen_data()
 {
-    python gen_train_data.py ../../gen_data/ijcai2016_koubei_trainNov ../../gen_data/trainSample 1
-    awk -F'\t' '{if($1==1)print $0}' ../../gen_data/trainSample > ../../gen_data/trainSample_positive
-    awk -F'\t' '{if($1==0)print $0}' ../../gen_data/trainSample > ../../gen_data/trainSample_negative
+    python gen_train_data.py ../../gen_data/ijcai2016_koubei_trainNov ../../gen_data/train_temp 1
+    awk -F'\t' '{if($1==1)print $0}' ../../gen_data/train_temp > ../../gen_data/trainSample_positive
+    awk -F'\t' '{if($1==0)print $0}' ../../gen_data/train_temp > ../../gen_data/trainSample_negative
     python gen_train_data.py ../../ori_data/ijcai2016_koubei_test ../../gen_data/testSample 2
 }
 
@@ -12,6 +12,7 @@ function Sample()
     negative_num=`cat ../../gen_data/trainSample_negative | wc -l`
     sample_num=$[ $positive_num * 2 ]
     python randomSampling.py $negative_num $sample_num ../../gen_data/trainSample_negative ../../gen_data/trainSample_negative_sampling
+    cat ../../gen_data/trainSample_positive ../../gen_data/trainSample_negative_sampling > ../../gen_data/trainSample
 }
 
 function Run()
