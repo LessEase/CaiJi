7ad5da4386a45eaeedce8c59f7c6fb1605c3a8bc
root <root@sjs_86_171.sogou-in.domain>
2016-05-05 14:29:58 +0800

    add split

--- code/conduct_data/run_gen_train_data.sh
+++ code/conduct_data/run_gen_train_data.sh
@@ -1,2 +1,4 @@
 python gen_train_data.py ../../gen_data/ijcai2016_koubei_trainNov ../../gen_data/trainSample 1
+awk -F'\t' '{if($1==1)print $0}' ../../gen_data/trainSample > ../../gen_data/trainSample_positive
+awk -F'\t' '{if($1==0)print $0}' ../../gen_data/trainSample > ../../gen_data/trainSample_negative
 python gen_train_data.py ../../ori_data/ijcai2016_koubei_test ../../gen_data/testSample 2
