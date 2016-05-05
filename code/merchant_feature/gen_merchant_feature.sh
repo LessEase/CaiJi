89f8c0f14e2bb7c4617cb9ff3efab38c7c2c36f3
root <root@sjs_86_171.sogou-in.domain>
2016-05-05 21:39:00 +0800

    repair

--- code/merchant_feature/gen_merchant_feature.sh
+++ code/merchant_feature/gen_merchant_feature.sh
@@ -1,4 +1,4 @@
 #python cxq_static_merchant_feature.py  infile  outfile 
 
-python cxq_static_merchant_feature.py ../../gen_data/ijcai2016_koubei_trainBefore11 ../../gen_data/cxq_koubei_merchant_feature_Before11
-python cxq_static_merchant_feature.py ../../gen_data/ijcai2016_koubei_trainAfter7 ../../gen_data/cxq_koubei_merchant_feature_After7
+python cxq_static_merchant_feature.py ../../gen_data/ijcai2016_koubei_trainBefore11 ../../gen_data/merchant_feature_Before11
+python cxq_static_merchant_feature.py ../../gen_data/ijcai2016_koubei_trainAfter7 ../../gen_data/merchant_feature_After7
