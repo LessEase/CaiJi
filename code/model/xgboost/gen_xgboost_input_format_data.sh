3948b4776e8425b867ad6483e0026be5a944df8e
LessEase <chshaoyi7@gmail.com>
2016-05-07 16:23:22 +0800

    fix bugs

--- code/model/xgboost/gen_xgboost_input_format_data.sh
+++ code/model/xgboost/gen_xgboost_input_format_data.sh
@@ -6,7 +6,7 @@
 #########################################################################
 #!/bin/bash
 
-#python transform2xgboost_input_formal.py  inputfile outputfile isTrain(1/0)
-python transform2xgboost_input_formal.py  ../../../gen_data/trainSample ../../../gen_data/xgboost_train_data 1
-python transform2xgboost_input_formal.py  ../../../gen_data/testSample ../../../gen_data/xgboost_test_data 0
+#python transform2xgboost_input_format.py  inputfile outputfile isTrain(1/0)
+python transform2xgboost_input_format.py  ../../../gen_data/trainSample ../../../gen_data/xgboost_train_data_1_to_5 1
+#python transform2xgboost_input_format.py  ../../../gen_data/testSample ../../../gen_data/xgboost_test_data 0
 
