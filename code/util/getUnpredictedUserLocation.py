1a63927678f591a8ff8c1940120007357fed03eb
LessEase <chshaoyi7@gmail.com>
2016-05-07 20:54:57 +0800

    get unpredicted user location tuple sets

---
+++ code/util/getUnpredictedUserLocation.py
@@ -0,0 +1,38 @@
+#/usr/bin/python
+#_*_coding: utf-8_*_
+
+import sys
+
+def getUnpredictedFile(predicted_file, all_file):
+
+	predicted_result = set()
+	not_predicted_result = set()
+	with open(predicted_file, "r") as fin:
+		for line in fin:
+			items = line.strip().split(",")
+			uid = items[0]
+			lid = items[1]
+			tpl = (uid, lid)
+			if tpl not in predicted_result:
+				predicted_result.add(tpl)
+
+	
+	with open(all_file, "r") as fin:
+		for line in fin:
+			items = line.strip().split(",")
+			uid = items[0]
+			lid = items[1]
+			tpl = (uid, lid)
+			if tpl not in predicted_result:
+				not_predicted_result.add(tpl)
+	
+	return not_predicted_result
+
+if __name__ == "__main__":
+
+	predicted_file = sys.argv[1]
+	all_file = sys.argv[2]
+	unpredicted_set = getUnpredictedFile(predicted_file, all_file)
+	print len(unpredicted_set)
+
+
