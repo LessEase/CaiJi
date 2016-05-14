0259ab2e00db9cba150bdfad14f88e94b54884ef
root <root@sjs_86_171.sogou-in.domain>
2016-05-14 17:00:06 +0800

    split data by time

--- code/util/split.sh
+++ code/util/split.sh
@@ -1,4 +1,5 @@
 # sh split.sh ijcai2016_koubei_train 4 20150700 20151100 outputfile
 
-awk -F',' '{if($4>"20151123" && $4<"20151200")print $0}' ../../gen_data/ijcai2016_koubei_trainNov > ../../gen_data/ijcai2016_koubei_trainNov_24_30
-awk -F',' '{if($4>"20151100" && $4<"20151124")print $0}' ../../gen_data/ijcai2016_koubei_trainNov > ../../gen_data/ijcai2016_koubei_trainNov_01_23
+awk -F',' '{if($4<"20151100")print $0}' ../validate/koubei_splited_train_data > ../validate/data/koubei_splited_train_data_Before11
+awk -F',' '{if($4>"20151100")print $0}' ../validate/koubei_splited_train_data > ../validate/data/koubei_splited_train_data_Nov
+awk -F',' '{if($4<"20151100")print $0}' ../validate/koubei_splited_val_data > ../validate/data/koubei_splited_val_data_Before11
