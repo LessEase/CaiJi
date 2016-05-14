eb3da788c22d5a2b54690aaf7f10f839490e4616
root <root@sjs_86_171.sogou-in.domain>
2016-05-14 15:45:10 +0800

    add

--- code/user_feature/gen_cxq_added_user_feature.sh
+++ code/user_feature/gen_cxq_added_user_feature.sh
@@ -1,2 +1,3 @@
 #cat ../../gen_data/   | python cxq_added_user_feature.py ../../gen_data/cxq_added_user_feature_Before11.pkl 1 (1:train; 2:test)
-cat ../../gen_data/10000.txt   | python cxq_added_user_feature.py ../../gen_data/cxq_added_user_feature_Before11.pkl 1
+cat ../../gen_data/ijcai2016_koubei_trainBefore11   | python cxq_added_user_feature.py ../../gen_data/cxq_added_user_feature_Before11.pkl 1
+cat ../../gen_data/ijcai2016_koubei_trainAfter7   | python cxq_added_user_feature.py ../../gen_data/cxq_added_user_feature_After7.pkl 2
